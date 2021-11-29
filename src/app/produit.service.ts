4889d0d92d2bc7e85fe359e09ec28c0999b4b30c
Aziz <aziz.mohamed10@outlook.com>
2021-11-29 02:15:18 +0100

    Application TP9 finalisé

--- src/app/produit.service.ts
+++ src/app/produit.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Produit } from './produit';
+import { Observable } from 'rxjs';
+
+const URL = 'http://localhost:3000/produits';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProduitService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProduits(): Observable<Produit[]> {
+    return this.http.get<Produit[]>(URL);
+  }
+
+  addProduit(p: Produit): Observable<Produit> {
+    return this.http.post<Produit>(URL, p);
+  }
+
 }
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Produit } from '../produit';
+import { ProduitService } from '../produit.service';
 
 @Component({
   selector: 'app-produits',
@@ -11,13 +12,20 @@ export class ProduitsComponent implements OnInit {
   lesproduits: Produit[]=[];
   prodForm:FormGroup= new FormGroup({});
  
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,private produitService:ProduitService) { }
+
+  onAjouter(){
+    this.produitService.addProduit(this.prodForm.value)
+    .subscribe (data => console.log(data));
+  }
 
   ngOnInit(): void {
     this.prodForm = this.fb.group({
       libelle:[''],
       prix:[]
-    })
+    });
+    this.produitService.getProduits()
+ .subscribe (data => this.lesproduits = data);
   }
 
 }
